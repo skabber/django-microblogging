7480fa61ca74d94de7442bf221095f167d241f60
skabber <skabber@c043426e-a16b-11dd-bc7a-dd6b2ed99ab5>
2008-11-13 03:27:39 +0000

    Forgot to bring over the urls.py file
    git-svn-id: http://django-microblogging.googlecode.com/svn/trunk@4 c043426e-a16b-11dd-bc7a-dd6b2ed99ab5

---
+++ microblogging/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    url(r'^$', 'microblogging.views.personal', name='tweets_you_follow'),
+    url(r'^all/$', 'microblogging.views.public', name='all_tweets'),
+    url(r'^(\d+)/$', 'microblogging.views.single', name='single_tweet'),
+
+    url(r'^followers/(\w+)/$', 'microblogging.views.followers', name='tweet_followers'),
+    url(r'^following/(\w+)/$', 'microblogging.views.following', name='tweet_following'),
+)
