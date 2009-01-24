5d013592f111cca354239128eac11dd2e953df25
skabber <skabber@c043426e-a16b-11dd-bc7a-dd6b2ed99ab5>
2009-01-24 16:39:40 +0000

    Added a setup.py file for pip support.
    git-svn-id: http://django-microblogging.googlecode.com/svn/trunk@16 c043426e-a16b-11dd-bc7a-dd6b2ed99ab5

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+version = '0.1.0'
+
+setup(
+    name='django-microblogging',
+    version=version,
+    description="django-microblogging",
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Environment :: Web Environment",
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords='microblogging,django',
+    author='James Tauber',
+    author_email='jtauber@jtauber.com',
+    url='http://code.google.com/p/django-microblogging/',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
